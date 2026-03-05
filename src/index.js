1c7485f217fd157f81f7b77f361667a8ba5bd34b
Oksana Shevchenko <o.v.shevchenko@student.csn.khai.edu>
2026-03-05 13:26:37 +0200

    LR3

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-const express = require('express')
+const express = require('express');
+
 const productRoutes = require('./product.routes');
+const userRouter = require('./user/user.router');
+
 const { logRequest } = require('./middleware');
+const bodyParser = require('body-parser');
 const { errorResponder } = require('./error.middleware');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(logRequest);
+
 app.use(productRoutes);
+app.use(userRouter);
+
 app.use(errorResponder);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
-
